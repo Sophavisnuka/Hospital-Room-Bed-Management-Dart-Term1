fbb7f124e255653e3ce1b8cdf67619cc92539ad3
Phay Someth <phay.someth70@gmail.com>
2025-10-23 23:56:09 +0700

    Refactor Person class to enforce required properties and improve toString method

--- lib/Domain/person.dart
+++ lib/Domain/person.dart
@@ -1,30 +1,36 @@
 abstract class Person {
   final String _name;
-  final int? _age;
+  final int _age;
+  final String _gender;
   final String? _phone;
   final String? _dateOfBirth;
 
   Person({
     required String name,
-    int? age,
-    String? phone,
-    String? dateOfBirth,
-    }) : _name = name,
-         _age = age, 
-         _phone = phone, 
-         _dateOfBirth = dateOfBirth;
+    required final int age,
+    required final String gender,
+    final String? phone,
+    final String? dateOfBirth,
+  })  : _name = name,
+        _age = age,
+        _gender = gender,
+        _phone = phone,
+        _dateOfBirth = dateOfBirth;
 
   String get name => _name;
-  int? get age => _age;
+  int get age => _age;
+  String get gender => _gender;
   String? get phone => _phone;
   String? get dateOfBirth => _dateOfBirth;
 
-  String displayInfo() {
-    final parts = <String>[];
-    parts.add('Name: $_name');
-    if (_age != null) parts.add('Age: $_age');
-    if (_phone != null) parts.add('Phone: $_phone');
-    if (_dateOfBirth != null) parts.add('DOB: $_dateOfBirth');
-    return parts.join(', ');
+  @override
+  String toString() {
+    return """
+    name: $_name, 
+    age: $_age, 
+    gender: $_gender, 
+    phone: $_phone, 
+    dateOfBirth: $_dateOfBirth,
+    """;
   }
-} 
+}
